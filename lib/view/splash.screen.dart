4767bc8a44eaf7001f3dbf3af8e5f447dc061075
Beatriz Ponces <51097911+BeatrizPonces@users.noreply.github.com>
2020-05-11 19:40:01 -0400

    splash.screen concluided

--- lib/view/splash.screen.dart
+++ lib/view/splash.screen.dart
@@ -11,68 +11,79 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(
-      Duration(seconds: 10),
-      () {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context)
-            => SelectPage()
-          )
-        );
-      }
-    );
+    Future.delayed(Duration(seconds: 10), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => SelectPage()));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance.collection('standard').document('splashScreen').snapshots(),
-        builder: (context, snapshot) {
-          switch(snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            default:
-              String background = snapshot.data['background'];
-              String title = snapshot.data['title'];
-              return Stack(
-                children: <Widget>[
-                  Image(
-                    image: NetworkImage(background),
-                    fit: BoxFit.cover,
-                    height: double.infinity,
-                  ),
-                  Center(
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                        fontSize: 28,
-                        color: Colors.white,
+          stream: Firestore.instance
+              .collection('standard')
+              .document('splashScreen')
+              .snapshots(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              default:
+                String background = snapshot.data['background'];
+                String title = snapshot.data['title'];
+                return Stack(
+                  children: <Widget>[
+                    Image(
+                      image: NetworkImage(background),
+                      fit: BoxFit.cover,
+                      height: double.infinity,
+                    ),
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Stack(
+                            children: <Widget>[
+                              // Stroked text as border.
+                              Text(
+                                title,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontFamily: "Marguerite",
+                                  foreground: Paint()
+                                    ..style = PaintingStyle.stroke
+                                    ..strokeWidth = 6
+                                    ..color = Colors.white,
+                                ),
+                              ),
+                              // Solid text as fill.
+                              Text(
+                                title,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.black,
+                                  fontFamily: "Marguerite",
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          CircularProgressIndicator(),
+                        ],
                       ),
                     ),
-                  ),
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SizedBox(
-                          height: 250,
-                        ),
-                        CircularProgressIndicator(),
-                      ],
-                    ),
-                  ),
-                 
-                ],
-              );
-          }
-        }
-      ),
+                  ],
+                );
+            }
+          }),
     );
   }
 }
