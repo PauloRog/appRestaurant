6024279d6f926b4c52d5aeda5272c610faf53144
victor <saitovictor36@gmail.com>
2020-05-12 10:40:40 -0400

    atualizacao login

--- lib/view/sheets/adm.login.page.dart
+++ lib/view/sheets/adm.login.page.dart
@@ -10,62 +10,69 @@ class AdmLogin extends StatefulWidget {
 class _AdmLogin extends State<AdmLogin> {
   @override
   Widget build(BuildContext context) {
-    return Stack(children: <Widget>[
-      Container(
-        color: Colors.blue,
-      ),
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Digite Sua Senha:",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.white,
-              ),
-            ),
-            Container(
-              child:TextField(
-                decoration: InputDecoration.collapsed(hintText: 'Blá Blá'),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              height: 60,
-              width: 185,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color.fromRGBO(156, 44, 12, 1),
-                    Color.fromRGBO(53, 10, 0, 1),
-                  ],
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.purple,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Digite Sua Senha:",
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  onPressed: () {
-                    print("pressionado demais");
-                  },
-                  child: Text(
-                    "Continuar",
-                    style: TextStyle(
-                      fontSize: 26,
-                      color: Colors.black,
+              Container(
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    hintText: "Senha",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                        fillColor: Color(0)
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 60,
+                width: 185,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromRGBO(113, 00, 00, 1),
+                      Color.fromRGBO(187, 98, 98, 1),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: SizedBox.expand(
+                  child: FlatButton(
+                    onPressed: () {
+                      print("pressionado demais");
+                    },
+                    child: Text(
+                      "Continuar",
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
-    ]);
+    );
   }
 }
