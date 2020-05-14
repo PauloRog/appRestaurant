f99b5fc3d6c402bdb58dbc1941a853711f41b98e
Beatriz Ponces <51097911+BeatrizPonces@users.noreply.github.com>
2020-05-14 13:19:03 -0400

    atualizar add

--- lib/functions/add.dart
+++ lib/functions/add.dart
@@ -2,13 +2,15 @@ import 'package:app_restaurant_test/models/products.dart';
 import 'package:app_restaurant_test/test/list.products.dart';
 
 class Add {
+  ListProducts list = ListProducts();
+
   void addFood(String image, String title, String description, double price) {
-    ListProducts.foods.add(Products(
+    list.foods.add(Products(
         image: image, title: title, description: description, price: price));
   }
 
   void addDrink(String image, String title, String description, double price) {
-    drinks.add(Products(
+    list.drinks.add(Products(
         image: image, title: title, description: description, price: price));
   }
 }
